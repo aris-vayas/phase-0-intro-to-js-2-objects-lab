ddbf6c33d37f201c155e351fc91ed305f20274bd
Aris Vayas <aris.vayas@gmail.com>
2022-04-05 13:38:26 -0400

    commit tree acaaa22126ad8d57ef076c9bd83cc9ca228c332f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee ={
+    Name:"Bob",
+    address:"New York",
+  }
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = { ...employee };
+  
+    newObj [key]=value;
+  
+    return newObj;
+  }
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+  
+    employee[key]= value
+    return employee
+  }
+  function deleteFromEmployeeByKey(employee,key,value){
+    const newEmployee = { ...employee };
+  
+    newEmployee[key] = value;
+  
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key,value){
+  employee[key] =value 
+  return employee
+  }
+  
+  
+  
